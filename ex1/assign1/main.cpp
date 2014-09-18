abc78c1ba55068b23013bdaff107108e516daa64
Jerry Webb <jerryweb@usc.edu>
2014-09-17 22:21:28 -0700

    got the camera working

--- ex1/assign1/main.cpp
+++ ex1/assign1/main.cpp
@@ -31,6 +31,7 @@ float g_vLandScale[3] = {1.0, 1.0, 1.0};
 /* see <your pic directory>/pic.h for type Pic */
 Pic * g_pHeightData;
 
+
 /* Write a screenshot to the specified filename */
 void saveScreenshot (char *filename)
 {
@@ -49,6 +50,9 @@ void saveScreenshot (char *filename)
         glReadPixels(0, 479-i, 640, 1, GL_RGB, GL_UNSIGNED_BYTE,
                      &in->pix[i*in->nx*in->bpp]);
     }
+    for(j=639; i>=0; i--){
+        glReadPixels(0, 480, 639-j, 1, GL_RGB, GL_UNSIGNED_BYTE, &in->pix[i*in->ny*in->bpp]);
+    }
     
     if (jpeg_write(filename, in))
         printf("File saved Successfully\n");
@@ -56,13 +60,18 @@ void saveScreenshot (char *filename)
         printf("Error in Saving\n");
     
     pic_free(in);
+    
+    
+    //PIC_PIXEL(filename, i, j, 0);
+    
+    
 }
 
 
 void myinit()
 {
     /* setup gl view here */
-    glClearColor(1.0, 1.0, 0.8, 1.0);
+    glClearColor(0.0, 0.0, 0.0, 0.0);
     glShadeModel(GL_SMOOTH);
 }
 
@@ -74,16 +83,16 @@ void setupCamera(){
 
 void reshape(int w, int h){
     
-    GLfloat aspect = (GLfloat) w / (GLfloat) h;
-    glViewport(0, 0, w, h);
-    glMatrixMode(GL_PROJECTION);
-    glLoadIdentity();
-    if (w <= h) /* aspect <= 1 */
-        glOrtho(-2.0, 2.0, -2.0/aspect, 2.0/aspect, -10.0, 10.0);
-    else /* aspect > 1 */
-        glOrtho(-2.0*aspect, 2.0*aspect, -2.0, 2.0, -10.0, 10.0);
-    glMatrixMode(GL_MODELVIEW);
-    
+//    GLfloat aspect = (GLfloat) w / (GLfloat) h;
+//    glViewport(0, 0, w, h);
+//    glMatrixMode(GL_PROJECTION);
+//    glLoadIdentity();
+//    if (w <= h) /* aspect <= 1 */
+//        glOrtho(-2.0, 2.0, -2.0/aspect, 2.0/aspect, -10.0, 10.0);
+//    else /* aspect > 1 */
+//        glOrtho(-2.0*aspect, 2.0*aspect, -2.0, 2.0, -10.0, 10.0);
+//    glMatrixMode(GL_MODELVIEW);
+//    
     
     //create image size
 //    glViewport(0, 0, w, h);
@@ -92,9 +101,9 @@ void reshape(int w, int h){
     
     //set the camera
     //glFrustum(-0.1, 0.1, -float(h)/(10.0*float(w)), float(h)/(10.0*float(w)), 0.5, 1000.0);
-    glFrustum(-0.3,0.3 , -640, 640, 0.5, 1000.0);
-    glMatrixMode(GL_MODELVIEW);
-    glLoadIdentity();
+//    //glFrustum(-0.3,0.3 , -640, 640, 0.5, 1000.0);
+////    glMatrixMode(GL_MODELVIEW);
+////    glLoadIdentity();
 }
 
 void triangle(){
@@ -105,31 +114,49 @@ void triangle(){
     
 	glColor3f(0.0, 0.0, 0.0);
     glVertex3f(-0.25, -0.25, 0.50);
-    //glColor3f(0.0, 0.0, 1.0);
+    glColor3f(0.0, 0.0, 1.0);
     glVertex3f(-0.25, 0.25, 0.50);
-    //glColor3f(0.0, 0.0, 0.0);
+    glColor3f(0.0, 0.0, 0.0);
     glVertex3f(0.25, 0.25, 0.0);
     
     glEnd();
     
 
 }
+
+void drawPoints(){
+    
+    
+        
+    glBegin(GL_LINES);
+    for(int i = 0; i<5;i++){
+        glColor3f(1.0, 1.0, 1.0);
+        glVertex3f(i/5, i/5, 0);
+        glColor3f(0.0, 0.0, 0.0);
+        glVertex3f(1 + i/5,1 + i/5, 0);
+        //    glColor3f(0.0, 0.0, 0.0);
+        //    glVertex3f(2, 0, 0);    }
+    
+    }
+    glEnd();
+}
+
 void shapeList(){
-    GLuint triangleList = glGenLists(1); //going to create 4 tirangles
-        glNewList(triangleList, GL_COMPILE);
-            glColor3f(1.0, 0.0, 0.0);
-            glBegin(GL_TRIANGLES);
-    //glColor3f(1.0, 1.0, 1.0);
-    glVertex3f(-0.5, -0.5, 1.0);
-    //glColor3f(0.0, 0.0, 1.0);
-    glVertex3f(-0.5, 0.5, 1.0);
-    //glColor3f(0.0, 0.0, 0.0);
-    glVertex3f(0.5, 0.5, 0.0);
-    //glColor3f(1.0, 1.0, 0.0);
-    //glVertex3f(0.5, -0.5, 0.0);
-            glEnd();
-        glEndList();
-        glCallList(triangleList);
+//    GLuint triangleList = glGenLists(1); //going to create 4 tirangles
+//        glNewList(triangleList, GL_COMPILE);
+//            glColor3f(1.0, 0.0, 0.0);
+//            glBegin(GL_TRIANGLES);
+//    //glColor3f(1.0, 1.0, 1.0);
+//    glVertex3f(-0.5, -0.5, 1.0);
+//    //glColor3f(0.0, 0.0, 1.0);
+//    glVertex3f(-0.5, 0.5, 1.0);
+//    //glColor3f(0.0, 0.0, 0.0);
+//    glVertex3f(0.5, 0.5, 0.0);
+//    //glColor3f(1.0, 1.0, 0.0);
+//    //glVertex3f(0.5, -0.5, 0.0);
+//            glEnd();
+//        glEndList();
+//        glCallList(triangleList);
     
     
 }
@@ -157,8 +184,8 @@ void polygon(){
 }
 
 //void cube(){
-    GLfloat vertices[8][3] = {{-1.0, -1.0, -1.0}, {1.0, -1.0, -1.0},
-        {1.0, 1.0, -1.0}, {-1.0, 1.0, -1.0}, {-1.0, -1.0, 1.0},
+    GLfloat vertices[8][3] = {{-1.0, -0.0, -1.0}, {1.0, -0.0, -1.0},
+        {1.0, 0.50, -1.0}, {-1.0, 0.5, -1.0}, {-1.0, -1.0, 1.0},
         {1.0, -1.0, 1.0}, {1.0, 1.0, 1.0}, {-1.0, 1.0, 1.0}};
     
     GLfloat colors[8][3] = {{0.0, 0.0, 0.0}, {1.0, 0.0, 0.0},
@@ -194,8 +221,9 @@ void colorcube(void)
 
 GLfloat theta[3] = {0.0, 0.0, 0.0};
 
-GLfloat delta = 2.0;
+GLfloat delta = 0.5;
 GLint axis = 2;
+
 void spinCube()
 {
     
@@ -216,18 +244,21 @@ void display()
     
     setupCamera();
     
-    //cube();
-    
     glLoadIdentity();
-    glRotatef(theta[0], 1.0, 0.0, 0.0);
-    glRotatef(theta[1], 0.0, 1.0, 0.0);
-    glRotatef(theta[2], 0.0, 0.0, 1.0);
-    colorcube();
-//    triangle();
-//    glLoadIdentity(); // reset transformation
-//    polygon();
+    
+    //gluLookAt(0.0, 0.0, 0.0, 0.0, 1.0, -100.0, 0.0, 1.0, 0.0);
+    glScaled(g_vLandScale[0], g_vLandScale[1], g_vLandScale[2]);
+    glTranslated(g_vLandTranslate[0], g_vLandTranslate[1], g_vLandTranslate[2]);
+    glRotatef(g_vLandRotate[0]/5, g_vLandRotate[0], g_vLandRotate[1], g_vLandRotate[2]);
+//    glRotatef(theta[0], 1.0, 0.0, 0.0);
+//    glRotatef(theta[1], 0.0, 1.0, 0.0);
+//    glRotatef(theta[2], 0.0, 0.0, 1.0);
+    //colorcube();
     //glLoadIdentity();
-    //shapeList();
+    drawPoints();
+  
+    //triangle();
+
 
     glutSwapBuffers(); // double buffer flush
 }
@@ -344,21 +375,34 @@ void mousebutton(int button, int state, int x, int y)
     g_vMousePos[0] = x;
     g_vMousePos[1] = y;
 }
+bool stop;
+
+void keyboard(unsigned char key, int x, int y)
+{
+    if (key=='q' || key == 'Q')
+        exit(0);
+    if (key==' ')
+        stop = !stop;
+    if (stop)
+        glutIdleFunc(doIdle);
+    else
+        glutIdleFunc(spinCube);
+}
 
 int main (int argc, char ** argv)
 {
-//    if (argc<2)
-//    {
-//        printf ("usage: %s heightfield.jpg\n", argv[0]);
-//        exit(1);
-//    }
-//    
-//    g_pHeightData = jpeg_read(argv[1], NULL);
-//    if (!g_pHeightData)
-//    {
-//        printf ("error reading %s.\n", argv[1]);
-//        exit(1);
-//    }
+    if (argc<2)
+    {
+        printf ("usage: %s heightfield.jpg\n", argv[0]);
+        exit(1);
+    }
+    
+    g_pHeightData = jpeg_read(argv[1], NULL);
+    if (!g_pHeightData)
+    {
+        printf ("error reading %s.\n", argv[1]);
+        exit(1);
+    }
     
     glutInit(&argc,argv);
     
@@ -366,7 +410,7 @@ int main (int argc, char ** argv)
     glutInitDisplayMode(GLUT_DOUBLE | GLUT_DEPTH | GLUT_RGBA);
     
     // set window size
-    glutInitWindowSize(640, 480);
+    glutInitWindowSize(1024, 800);
     
     // set window position
     glutInitWindowPosition(0, 0);
@@ -400,8 +444,10 @@ int main (int argc, char ** argv)
     /* callback for idle mouse movement */
     glutPassiveMotionFunc(mouseidle);
     /* callback for mouse button changes */
-    //glutMouseFunc(mousebutton);
-    glutMouseFunc(mouseForCubeSpin);
+    glutMouseFunc(mousebutton);
+    //glutMouseFunc(mouseForCubeSpin);
+    
+    glutKeyboardFunc(keyboard);
     
     /* do initialization */
     myinit();
